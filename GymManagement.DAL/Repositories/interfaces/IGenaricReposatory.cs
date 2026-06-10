8d76d146220fac60fb2f7df6aaaf46c966704efc
rahmaasy17 <rahmaasy17@gmail.cim>
2026-06-10 22:59:48 +0200

    Add project files.

---
+++ GymManagement.DAL/Repositories/interfaces/IGenaricReposatory.cs
@@ -0,0 +1,20 @@
+using GymManagement.DAL.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL.Repositories.interfaces
+{
+    public interface IGenaricReposatory<TEntity> where TEntity : BaseEntity , new()
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool tracking = false, CancellationToken c = default);
+        Task<TEntity?> GetByIDAsync(int id, CancellationToken c = default);
+        Task<int> AddAsync(TEntity entity);
+        Task<int> UpdateAsync(TEntity entity);
+
+        Task<int> DeleteAsync(TEntity entity);
+    }
+}
---
+++ GymManagement/Program.cs
@@ -0,0 +1,50 @@
+using GymManagement.DAL.Data.DbContexts;
+using GymManagement.DAL.Repositories.classes;
+using GymManagement.DAL.Repositories.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //to create obj of container , get actions and views
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            //when he ask for IPlanRepository sent PlanRepository
+            // builder.Services.AddScoped<IPlanRepository, PlanRepository>();
+            builder.Services.AddScoped(typeof(IGenaricReposatory<>),typeof(GenaricRepository<>));
+            builder.Services.AddDbContext<GymDbContext>(options => {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                });
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
